25fadb2e422980dbb08cb761daf5d80f321c86e3
ngonzalez <nestor.ge222@gmail.com>
2022-09-06 22:58:46 -0600

    [add] update to functional components

--- monster-rolodex/src/App.js
+++ monster-rolodex/src/App.js
@@ -1,63 +1,62 @@
-import { Component } from "react";
+import React, { useState, useEffect } from "react";
 
-import CardList from './components/card-list/card-list.component'
+import CardList from "./components/card-list/card-list.component";
 
 import "./App.css";
 import SearchBox from "./components/serach-box/search-box.component";
+/**.
+ *
+ * purre function
+ * return the exac same thing all the time
+ * const pureFunc =(a,b)=>{return a+b}
+ *
+ * inpure function
+ * lec c =4;
+ * const pureFunc =(a,b)=>{return a+b+c }
+ *
+ * side effects
+ * c = 3
+ * const funcB = (a,b)=>{ c=a+b return a*B}
+ * the c is updated ouside of whis scope, an the params are pase and update some thing
+ *
+ * se recomineda programar en programacion con funciones puras
+ */
 
-class App extends Component {
-  constructor() {
-    console.log('component constructor')
-    super();
+const App = () => {
+  const [searchFiled, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  const [filteredMonsters, setFilteredMonsters] = useState(monsters)
 
-    this.state = {
-      monsters: [],
-      searchFiled: "",
-    };
-  }
-
-  componentDidMount() {
-    console.log('component mount')
+  const onSearchChange = (event) => {
+    const field = event.target.value.toLowerCase();
+    setSearchField(field);
+  };
+  useEffect(()=>{
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) =>
-        this.setState(
-          () => {
-            return { monsters: users };
-          }
-          
-        )
-      );
-  }
-  onSearchChange = (event) => {
-    const searchFiled = event.target.value.toLowerCase();
+      .then((users) => setMonsters(users));
+  } ,[])
 
-    this.setState(
-      () => {
-        return { searchFiled };
-      }
-    );
-  }
-
-  render() {
-    console.log('render')
-
-    const {monsters, searchFiled} = this.state
-    const { onSearchChange } = this
-
-    const filteredMonster = monsters.filter((monster) => {
+  useEffect(()=>{
+    const newfilteredMonster = monsters.filter((monster) => {
       return monster.name.toLowerCase().includes(searchFiled);
     });
 
-    return (
-      <div className="App">
-        <h1 className="app-title">Monster rolodex</h1>
-        
-        <SearchBox onChangeHandler={onSearchChange} placeholder = "search monsters" className = 'search-b0x'/>
-        <CardList monsters = {filteredMonster}/>
-      </div>
-    );
-  }
-}
+    setFilteredMonsters(newfilteredMonster);
+  }, [monsters, searchFiled])
+
+
+  return (
+    <div className="App">
+      <h1 className="app-title">Monster rolodex</h1>
+      <SearchBox
+        onChangeHandler={onSearchChange}
+        placeholder="search monsters"
+        className="search-b0x"
+      />
+      <CardList monsters={filteredMonsters} />
+    </div>
+  );
+};
 
 export default App;
