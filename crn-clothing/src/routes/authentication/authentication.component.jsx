616800a613266bfcad2a1bfcabcb27a5714f05a6
Nestor Gonzalez <nestor.ge222@gmail.com>
2022-09-16 06:22:13 -0600

    [FIX] problem with button on render

--- crn-clothing/src/routes/authentication/authentication.component.jsx
+++ crn-clothing/src/routes/authentication/authentication.component.jsx
@@ -3,14 +3,12 @@ import SignUpForm from "../../components/sign-up/sign-up.component";
 import SignInForm from "../../components/sign-in/sign-in.component";
 
 const Autentication = () => {
-
-  
   return (
     <div>
       <h1>Sign In </h1>
-       <SignUpForm />
- {/*      <SignInForm />
-  */}    </div>
+      <SignUpForm />
+      <SignInForm />
+    </div>
   );
 };
 
